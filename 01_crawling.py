bd0df83445c65510a89708cb47bec1e46a5c4433
cswniw <cswniw@gmail.com>
2021-12-08 16:51:27 +0900

    crawling_01

--- 01_crawling.py
+++ 01_crawling.py
@@ -1,3 +1,38 @@
-print('hi')
+# https://github.com/cswniw/recommend_attraction
 
-print('test')
+from selenium import webdriver
+import pandas as pd
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+import time
+from selenium.webdriver.common.keys import Keys
+
+options = webdriver.ChromeOptions()
+options.add_argument('lang=ko_KR')
+options.add_argument('disable_gpu')
+driver = webdriver.Chrome('./chromedriver', options=options)
+
+
+for i in range(1,101) :
+    url = 'https://kr.trip.com/travel-guide/city-100042/tourist-attractions/{}.html'.format(i)
+    driver.get(url)
+
+    attraction_list = []
+    review_list = []
+    for j in range(1,11) :
+        content_title_xpath = '//*[@id="list"]/div[5]/div[{}]/a/div[2]/h3'.format(j)
+        attraction_xpath = '//*[@id="poi.detail.overview"]/div/div[1]/h1'
+        try :
+            time.sleep(1)
+            driver.find_element_by_xpath(content_title_xpath).click()
+            attraction = driver.find_element_by_xpath(attraction_xpath).text
+            attraction_list.append(attraction)
+            print(attraction)
+            driver.back()
+        except :
+            time.sleep(1)
+            driver.find_element_by_xpath(content_title_xpath).click()
+            attraction = driver.find_element_by_xpath(attraction_xpath).text
+            attraction_list.append(attraction)
+            print(attraction)
+            driver.back()
+
