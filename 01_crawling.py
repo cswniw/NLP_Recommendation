fd1146aad1f4e2161d326e48b4e585d659bc6723
cswniw <cswniw@gmail.com>
2021-12-08 17:02:49 +0900

    crawling_02

--- 01_crawling.py
+++ 01_crawling.py
@@ -17,22 +17,36 @@ for i in range(1,101) :
     driver.get(url)
 
     attraction_list = []
+    area_list = []
     review_list = []
     for j in range(1,11) :
         content_title_xpath = '//*[@id="list"]/div[5]/div[{}]/a/div[2]/h3'.format(j)
         attraction_xpath = '//*[@id="poi.detail.overview"]/div/div[1]/h1'
+        area_xpath = '//*[@id="__next"]/div[2]/div/div/div[2]/nav/div[5]/a'
         try :
             time.sleep(1)
             driver.find_element_by_xpath(content_title_xpath).click()
+
             attraction = driver.find_element_by_xpath(attraction_xpath).text
             attraction_list.append(attraction)
-            print(attraction)
+
+            area = driver.find_element_by_xpath(area_xpath).text
+            area_list.append(area)
+
+            print(attraction, area)
             driver.back()
         except :
+            print('except')
             time.sleep(1)
             driver.find_element_by_xpath(content_title_xpath).click()
             attraction = driver.find_element_by_xpath(attraction_xpath).text
             attraction_list.append(attraction)
-            print(attraction)
+            print(attraction, area)
             driver.back()
 
+
+# \<li class="number active">1</li>
+#
+# // *[ @ id = "reviews"] / div / div / div / div[3] / div[2] / div / div / div / ul / li[3]
+# // *[ @ id = "reviews"] / div / div / div / div[3] / div[2] / div / div / div / ul / li[2]
+# // *[ @ id = "reviews"] / div / div / div / div[3] / div[2] / div / div / div / ul / li[1]
